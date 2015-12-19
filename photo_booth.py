217605ff9a729c1eb03603e4ae24cd379f1400e3
Santina <s@croniser.com>
2015-12-19 00:43:09 -0600

    Update photo_booth.py

--- photo_booth.py
+++ photo_booth.py
@@ -3,6 +3,9 @@
 import RPi.GPIO as GPIO, time, os, subprocess
 import picamera
 import time
+import cups
+
+conn = cups.Connection()
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
@@ -40,12 +43,12 @@ while True:
       GPIO.output(POSE_LED, False)
       print("SNAP")
 
-      gpout = subprocess.check_output("raspistill -o /home/pi/photobooth_images/gala{timestamp:%Y-%m-%d-%H-%M}.jpg", stderr=subprocess.STDOUT, shell=True)
+      gpout = subprocess.check_output("raspistill -o /home/pi/photobooth_images/qfc/{time.strftime("%H:%M:%S")}.jpg", stderr=subprocess.STDOUT, shell=True)
       print(gpout)
       if "ERROR" not in gpout: 
         snap += 1
       GPIO.output(POSE_LED, False)
-      time.sleep(0.5)
+      time.sleep(0.2)
     print("please wait while your photos print...")
     GPIO.output(PRINT_LED, True)
     # build image and send to printer
@@ -53,6 +56,12 @@ while True:
     # TODO: implement a reboot button
     # Wait to ensure that print queue doesn't pile up
     # TODO: check status of printer instead of using this arbitrary wait time
+
+    # this should find the print queue and return the job state
+    #conn.getJobAttributes(printer_returns)["job-state"]
+    #but I'm not sure how to check to see if it equals 9 - job completed
+    #equals 9 (IPP_JOB_COMPLETED)
+    # if the print queue doesn't work, use this: 
     time.sleep(110)
     print("ready for next round")
     GPIO.output(PRINT_LED, False)
